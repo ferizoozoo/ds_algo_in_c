457a3c956848d8f606fc59509f46b4cc83a7c74a
ferizoozoo <farhadzohoorz@gmail.com>
2023-11-10 16:11:01 +0330

    Write insert_element function for dynamic array

--- dynamic_array/array.c
+++ dynamic_array/array.c
@@ -1,24 +1,56 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+
 #include "array.h"
 
 array* initialize_array(int size) {
-    array* array = malloc(sizeof(array));
+    array* arr = malloc(sizeof(array));
 
-    array->capacity = size + 10;
-    array->size = size;
-    array->list = malloc(array->capacity * sizeof(void*));
+    if (arr == NULL) {
+        printf("Couldn't allocate memory for array structure.");
+        exit(0);
+    }
 
-    return array;
+    arr->capacity = size + 10;
+    arr->size = size;
+    arr->list = malloc(arr->capacity * sizeof(void*));
+
+    if (arr == NULL) {
+        printf("Couldn't allocate memory for array.");
+        exit(0);
+    }
+
+    return arr;
 }
 
-array* insert(void* data) {
-    //TODO: check if capacity > size, then realloc memory
-    //      else array->list[array->size++] = data
+array* insert_element(array* arr, void* data) {
+    if (arr->capacity / 2 < arr->size) {
+        arr->capacity *= 2;
+        void** new_list = realloc(arr->list, arr->capacity * sizeof(void*));
+
+        if (new_list == NULL) {
+            printf("Couldn't reallocate memory for array.");
+            exit(0);
+        }
+
+        arr->list = new_list;
+    }
+
+    arr->list[arr->size++] = data;
+    return NULL;
 }
 
-array* remove_element(int index) {
-    
+array* remove_element(array* arr, int index) {
+    return NULL;
 }
 
-array* delete_array() {
+array* delete_array(array* arr) {
+    return NULL;
+}
 
+void print_array(array* arr) {
+    for (uint32_t i = 0; i < arr->size; i++) {
+        printf("%x\n", arr->list[i]);
+    }
 }
--- dynamic_array/array.h
+++ dynamic_array/array.h
@@ -1,10 +1,11 @@
 typedef struct {
     int size;
     int capacity;
-    void* list;
+    void** list;
 } array;
 
 array* initialize_array(int size);
-array* insert(void* data);
-array* remove_element(int index);
-array* delete_array();
+array* insert_element(array* arr, void* data);
+array* remove_element(array* arr, int index);
+array* delete_array(array* arr);
+void print_array(array* arr);
